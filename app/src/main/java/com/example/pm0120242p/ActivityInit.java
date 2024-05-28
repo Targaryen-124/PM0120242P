a724009eef4146ffe4d632540b9b7f87b39416af
Targaryen-124 <ypadilla1988@gmail.com>
2024-05-27 21:02:12 -0600

    Clase 270524

--- app/src/main/java/com/example/pm0120242p/ActivityInit.java
+++ app/src/main/java/com/example/pm0120242p/ActivityInit.java
@@ -1,6 +1,10 @@
 package com.example.pm0120242p;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +14,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ActivityInit extends AppCompatActivity {
 
+    EditText nombres, apellidos, edad, correo;
+
+    Button btningresar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +27,24 @@ public class ActivityInit extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        nombres = (EditText) findViewById(R.id.nombres);
+        apellidos = (EditText) findViewById(R.id.apellidos);
+        edad = (EditText) findViewById(R.id.edad);
+        correo = (EditText) findViewById(R.id.correo);
+        btningresar = (Button) findViewById(R.id.btningresar);
+
+        btningresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Agregar();
+            }
+        });
     }
+
+    private void Agregar()
+    {
+        Toast.makeText(this,"Hola como estas",Toast.LENGTH_LONG).show();
+    }
+
 }
